3a5725784aa3dc2b8e988150b764546356fc8a84
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-16 16:33:16 +0200

    模板

--- UnityABStudio/Extensions/ExportExtension.cs
+++ UnityABStudio/Extensions/ExportExtension.cs
@@ -1,4 +1,5 @@
 namespace SoarCraft.QYun.UnityABStudio.Extensions {
+    using System;
     using System.IO;
     using AssetReader.Entities.Enums;
     using AssetReader.Unity3D.Objects.Texture2Ds;
@@ -7,39 +8,40 @@ namespace SoarCraft.QYun.UnityABStudio.Extensions {
     using Services;
 
     public static class ExportExtension {
-        private static string path;
-        private static AssetItem asset;
+        private static string exportPath;
+        private static AssetItem item;
         private static readonly SettingsService settings = Ioc.Default.GetRequiredService<SettingsService>();
 
-        public static bool ExportConvertFile(this AssetItem asset, string path) {
-            ExportExtension.path = path;
-            ExportExtension.asset = asset;
+        public static bool ExportConvertFile(this AssetItem item, string exportPath) {
+            ExportExtension.exportPath = exportPath;
+            ExportExtension.item = item;
 
-            switch (asset.Type) {
-                case ClassIDType.Texture2D:
-                    return ExportTexture2D(asset, path);
-                case ClassIDType.AudioClip:
-                case ClassIDType.Shader:
-                case ClassIDType.TextAsset:
-                case ClassIDType.MonoBehaviour:
-                case ClassIDType.Font:
-                case ClassIDType.Mesh:
-                case ClassIDType.VideoClip:
-                case ClassIDType.MovieTexture:
-                case ClassIDType.Sprite:
-                case ClassIDType.Animator:
-                case ClassIDType.AnimationClip:
-                    return false;
-                default:
-                    return false; //
-            }
+            return item.Type switch {
+                ClassIDType.AnimationClip => false,
+                ClassIDType.Animator => ExportAnimator(),
+                ClassIDType.AudioClip => ExportAudioClip(),
+                ClassIDType.Texture2D => ExportTexture2D(),
+                ClassIDType.Font => ExportFont(),
+                ClassIDType.Mesh => ExportMesh(),
+                ClassIDType.MonoBehaviour => ExportMonoBehaviour(),
+                ClassIDType.MovieTexture => ExportMovieTexture(),
+                ClassIDType.Shader => ExportShader(),
+                ClassIDType.Sprite => ExportSprite(),
+                ClassIDType.TextAsset => ExportTextAsset(),
+                ClassIDType.VideoClip => ExportVideoClip(),
+                _ => ExportRawFile(),
+            };
         }
 
-        public static bool ExportTexture2D(this AssetItem asset, string path) {
-            var m_Texture2D = (Texture2D)asset.Obj;
+        private static bool ExportAnimator() => throw new NotImplementedException();
+
+        private static bool ExportAudioClip() => throw new NotImplementedException();
+
+        private static bool ExportTexture2D() {
+            var m_Texture2D = (Texture2D)item.Asset;
             if (settings.ConvertTexture) {
                 var type = settings.ConvertType;
-                if (!TryExportFile(path, asset, "." + type.ToString().ToLower(), out var exportFullPath))
+                if (!TryExportFile(exportPath, item, "." + type.ToString().ToLower(), out var exportFullPath))
                     return false;
                 var stream = m_Texture2D.ConvertToStream(type, true);
                 if (stream == null)
@@ -49,11 +51,21 @@ namespace SoarCraft.QYun.UnityABStudio.Extensions {
                     return true;
                 }
             } else {
-                if (!TryExportFile(path, asset, ".tex", out var exportFullPath))
+                if (!TryExportFile(exportPath, item, ".tex", out var exportFullPath))
                     return false;
                 File.WriteAllBytes(exportFullPath, m_Texture2D.image_data.GetData());
                 return true;
             }
         }
+
+        private static bool ExportFont() => throw new NotImplementedException();
+        private static bool ExportMesh() => throw new NotImplementedException();
+        private static bool ExportMonoBehaviour() => throw new NotImplementedException();
+        private static bool ExportMovieTexture() => throw new NotImplementedException();
+        private static bool ExportShader() => throw new NotImplementedException();
+        private static bool ExportSprite() => throw new NotImplementedException();
+        private static bool ExportTextAsset() => throw new NotImplementedException();
+        private static bool ExportVideoClip() => throw new NotImplementedException();
+        private static bool ExportRawFile() => throw new NotImplementedException();
     }
 }
