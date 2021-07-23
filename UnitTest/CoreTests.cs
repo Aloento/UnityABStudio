2e11bfa138db1a99b18a9cff4a0b3654104875d9
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-23 13:46:16 +0200

    FMODDecodeTest

--- UnitTest/CoreTests.cs
+++ UnitTest/CoreTests.cs
@@ -1,63 +1,25 @@
 namespace SoarCraft.QYun.UnityABStudio.UnitTest {
-    using System;
-    using System.IO;
-    using System.Runtime.InteropServices;
+    using AssetReader.Unity3D.Objects;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Core.FMOD;
     using Helpers;
 
     [TestClass]
     public class CoreTests {
-        [TestMethod]
-        public void FMODDecodeTest() {
-            var a = new AudioClipConverter(null);
+        [DataTestMethod]
+        [DataRow("Assets/m_bat_exterminate_intro.ab", 4816768734469872508)]
+        public void FMODDecodeTest(string filePath, long PathID) {
+            new AssetReaderTests().TryGetObjectByID(filePath, PathID, out var obj);
+            if (obj is not AudioClip)
+                Assert.Fail($"{PathID} 不是 {nameof(AudioClip)}");
 
-            var m_AudioData = File.ReadAllBytes("Assets/TestAudio.bin");
-            if (m_AudioData.Length == 0)
-                Assert.Fail("文件读取失败");
-
-            var exinfo = new CREATESOUNDEXINFO();
-            var result = Factory.System_Create(out var system);
-            if (result != RESULT.OK)
-                Assert.Fail("创建实例失败");
-
-            result = system.init(1, INITFLAGS.NORMAL, IntPtr.Zero);
-            if (result != RESULT.OK)
-                Assert.Fail("初始化失败");
-
-            Assert.AreEqual(8353696, m_AudioData.Length, $"数组大小不正常: {m_AudioData.Length}");
-            exinfo.cbsize = Marshal.SizeOf(exinfo);
-            exinfo.length = (uint)m_AudioData.Length;
-
-            result = system.createSound(m_AudioData, MODE.OPENMEMORY, ref exinfo, out var sound);
-            if (result != RESULT.OK)
-                Assert.Fail("创建声音失败");
-
-            result = sound.getNumSubSounds(out var numsubsounds);
-            if (result != RESULT.OK)
-                Assert.Fail("获得子声音数量失败");
-
-            byte[] buff;
-            if (numsubsounds > 0) {
-                result = sound.getSubSound(0, out var subsound);
-                if (result != RESULT.OK)
-                    Assert.Fail("获得子声音失败");
-
-                _ = buff = a.SoundToWav(subsound);
-                _ = subsound.release();
-            } else {
-                _ = buff = a.SoundToWav(sound);
+            if (!ToFile.ByteArrayToFile(@"C:\CaChe\UnityABStudio_AudioResult.wav",
+                new AudioClipConverter((AudioClip)obj).ConvertToWav())) {
+                Assert.Fail("导出失败");
             }
-
-            _ = sound.release();
-            _ = system.release();
-
-            Assert.AreEqual(true, ToFile.ByteArrayToFile(@"C:\CaChe\UnityABStudio_AudioResult.wav", buff), "导出失败");
         }
 
         [TestMethod]
         public void TextureDecodeTest() {
-
         }
     }
 }
