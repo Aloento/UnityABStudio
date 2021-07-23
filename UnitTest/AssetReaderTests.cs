82ebf7dde69855ff19ed2696b91f9003c342a05c
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-23 11:28:35 +0200

    撤

--- UnitTest/AssetReaderTests.cs
+++ UnitTest/AssetReaderTests.cs
@@ -1,4 +1,5 @@
 namespace SoarCraft.QYun.UnityABStudio.UnitTest {
+    using System;
     using System.IO;
     using AssetReader;
     using AssetReader.Unity3D;
@@ -7,21 +8,24 @@ namespace SoarCraft.QYun.UnityABStudio.UnitTest {
     [TestClass]
     public class AssetReaderTests {
         [DataTestMethod]
-        [DataRow("Assets/char_1012_skadi2.ab")]
-        public void LoadAssetTest(string filePath) {
-            var manager = new AssetsManager();
-            manager.LoadFilesAsync(Path.GetFullPath(filePath)).Wait();
+        [DataRow("Assets/char_1012_skadi2.ab", null)]
+        [DataRow("Assets/m_bat_exterminate_intro.ab", null)]
+        public void LoadAssetTest(string filePath, out AssetsManager outManager) {
+            Console.WriteLine($"{filePath}");
+            outManager = new AssetsManager();
+            outManager.LoadFilesAsync(Path.GetFullPath(filePath)).Wait();
         }
 
         [DataTestMethod]
         [DataRow("Assets/char_1012_skadi2.ab", 2938589673199698669, null)]
         public void TryGetObjectByID(string filePath, long PathID, out UObject outObj) {
-            var manager = new AssetsManager();
-            manager.LoadFilesAsync(Path.GetFullPath(filePath)).Wait();
+            LoadAssetTest(filePath, out var manager);
 
             foreach (var serializedFile in manager.AssetsFileList) {
                 if (serializedFile.ObjectsDic.TryGetValue(PathID, out var obj)) {
                     outObj = obj;
+                    Console.WriteLine($"{outObj}");
+                    return;
                 }
             }
 
