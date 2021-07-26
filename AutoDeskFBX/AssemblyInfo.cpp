e7f1e26d239608dc40eb00346cd7adc658e27a55
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-26 03:49:47 +0200

    AutoDeskFBX

---
+++ AutoDeskFBX/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"AutoDeskFBX")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"AutoDeskFBX")];
+[assembly:AssemblyCopyrightAttribute(L"版权所有(c)  2021")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute(L"1.0.*")];
+
+[assembly:ComVisible(false)];
---
+++ AutoDeskFBX/AutoDeskFBX.h
@@ -0,0 +1,10 @@
+#pragma once
+
+using namespace System;
+
+namespace SoarCraft::QYun::AutoDeskFBX {
+	public ref class FBX
+	{
+		// TODO: 在此处为此类添加方法。
+	};
+}
