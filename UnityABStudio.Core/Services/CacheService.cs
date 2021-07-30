ed6cbd615c8020322c2aaae3f43729a9a183aa75
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-30 18:51:00 +0200

    文件缓存

--- UnityABStudio.Core/Services/CacheService.cs
+++ UnityABStudio.Core/Services/CacheService.cs
@@ -3,6 +3,7 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
     using System.IO;
     using System.Security.Cryptography;
     using System.Threading.Tasks;
+    using AssetReader;
     using Microsoft.Extensions.Caching.Memory;
 
     public class CacheService {
@@ -10,14 +11,37 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
 
         public CacheService(IMemoryCache cache) => this.cache = cache;
 
-        public bool IsFileCached(string filePath) {
-            return false;
+        public async Task<bool> TryCacheFileAsync(SerializedFile serialized) {
+            var fullPath = Path.GetFullPath(serialized.originalPath);
+            var (res, _) = await IsFileCached(fullPath);
+            if (res)
+                return true;
+
+            return await this.TryPutAsync(fullPath,
+                new BundleCache(File.OpenRead(fullPath).Length, await this.GetFileMD5Async(fullPath), serialized));
         }
 
-        public async Task<string> GetFileMD5Async(string filePath) {
+        public async Task<(bool, SerializedFile)> TryGetCachedFileAsync(string filePath) {
             if (!File.Exists(filePath))
-                return string.Empty;
+                return (false, null);
 
+            var (res, file) = await IsFileCached(Path.GetFullPath(filePath));
+            return res ? (true, file) : (false, null);
+        }
+
+        private async Task<(bool, SerializedFile)> IsFileCached(string filePath) {
+            var (res, cache) = await TryGetValue<BundleCache>(filePath);
+            if (res) {
+                if (cache.Size == File.OpenRead(filePath).Length) {
+                    if (cache.MD5 == await GetFileMD5Async(filePath))
+                        return (true, cache.Serialized);
+                }
+            }
+
+            return (false, null);
+        }
+
+        private async Task<string> GetFileMD5Async(string filePath) {
             Stream stream = File.OpenRead(filePath);
             if (stream.Length > 1000000) {
                 var tmp = new byte[1000000];
@@ -40,5 +64,17 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
             this.cache.TryGetValue(key, out var res) ? (true, res as T) : (false, null));
 
         public void Remove(object key) => this.cache.Remove(key);
+    }
+
+    internal class BundleCache {
+        public long Size;
+        public string MD5;
+        public SerializedFile Serialized;
+
+        public BundleCache(long size, string md5, SerializedFile serialized) {
+            this.Size = size;
+            this.MD5 = md5;
+            this.Serialized = serialized;
+        }
     }
 }
