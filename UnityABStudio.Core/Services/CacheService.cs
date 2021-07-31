e434796d2ad046b7f239909d4ce1cc19b1ef0ec8
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-31 03:11:37 +0200

    FileInfo

--- UnityABStudio.Core/Services/CacheService.cs
+++ UnityABStudio.Core/Services/CacheService.cs
@@ -13,35 +13,36 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
 
         public async Task<bool> TryCacheFileAsync(SerializedFile serialized) {
             var fullPath = Path.GetFullPath(serialized.originalPath);
-            var (res, _) = await IsFileCached(fullPath);
-            if (res)
+            if (await this.IsFileCached(fullPath) != null)
                 return true;
 
             return await this.TryPutAsync(fullPath,
-                new BundleCache(File.OpenRead(fullPath).Length, await this.GetFileMD5Async(fullPath), serialized));
+                new BundleCache(new FileInfo(fullPath).Length, await this.GetFileMD5Async(fullPath), serialized),
+                false);
         }
 
-        public async Task<(bool, SerializedFile)> TryGetCachedFileAsync(string filePath) {
-            if (!File.Exists(filePath))
-                return (false, null);
+        public async Task<SerializedFile> TryGetCachedFileAsync(string filePath) => !File.Exists(filePath)
+            ? null
+            : await this.IsFileCached(Path.GetFullPath(filePath));
 
-            var (res, file) = await IsFileCached(Path.GetFullPath(filePath));
-            return res ? (true, file) : (false, null);
-        }
-
-        private async Task<(bool, SerializedFile)> IsFileCached(string filePath) {
-            var (res, cache) = await TryGetValue<BundleCache>(filePath);
-            if (res) {
-                if (cache.Size == File.OpenRead(filePath).Length) {
-                    if (cache.MD5 == await GetFileMD5Async(filePath))
-                        return (true, cache.Serialized);
+        private async Task<SerializedFile> IsFileCached(string filePath) {
+            var res = await TryGetValue<BundleCache>(filePath);
+            if (res != null) {
+                if (res.Size == new FileInfo(filePath).Length) {
+                    if (res.MD5 == await GetFileMD5Async(filePath))
+                        return res.Serialized;
                 }
             }
 
-            return (false, null);
+            return null;
         }
 
-        private async Task<string> GetFileMD5Async(string filePath) {
+#if DEBUG
+        public
+#else
+        private
+#endif
+            async Task<string> GetFileMD5Async(string filePath) {
             Stream stream = File.OpenRead(filePath);
             if (stream.Length > 1000000) {
                 var tmp = new byte[1000000];
@@ -52,29 +53,32 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
             return BitConverter.ToString(await MD5.Create().ComputeHashAsync(stream)).Replace("-", "");
         }
 
-        public Task<bool> TryPutAsync(object key, object value, int seconds = byte.MaxValue) => Task.Run(() => {
-            if (this.cache.TryGetValue(key, out _))
-                return false;
+        public Task<bool> TryPutAsync(object key, object value, bool checkExist = true, int seconds = byte.MaxValue) =>
+            Task.Run(() => {
+                if (checkExist) {
+                    if (this.cache.TryGetValue(key, out _))
+                        return false;
+                }
 
-            _ = this.cache.Set(key, value, TimeSpan.FromSeconds(seconds));
-            return true;
-        });
+                _ = this.cache.Set(key, value, TimeSpan.FromSeconds(seconds));
+                return true;
+            });
 
-        public Task<(bool, T)> TryGetValue<T>(object key) where T : class => Task.Run(() =>
-            this.cache.TryGetValue(key, out var res) ? (true, res as T) : (false, null));
+        public Task<T> TryGetValue<T>(object key) where T : class =>
+            Task.Run(() => this.cache.TryGetValue(key, out var res) ? res as T : null);
 
         public void Remove(object key) => this.cache.Remove(key);
-    }
 
-    internal class BundleCache {
-        public long Size;
-        public string MD5;
-        public SerializedFile Serialized;
+        private class BundleCache {
+            public long Size;
+            public string MD5;
+            public SerializedFile Serialized;
 
-        public BundleCache(long size, string md5, SerializedFile serialized) {
-            this.Size = size;
-            this.MD5 = md5;
-            this.Serialized = serialized;
+            public BundleCache(long size, string md5, SerializedFile serialized) {
+                this.Size = size;
+                this.MD5 = md5;
+                this.Serialized = serialized;
+            }
         }
     }
 }
