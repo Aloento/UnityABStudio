7f6da95e3c4380fa72e430915a45ca1deaf48bdd
Aloento <11802769+soarteam@users.noreply.github.com>
2021-07-31 03:11:37 +0200

    FileInfo

--- UnityABStudio.Core/Services/CacheService.cs
+++ UnityABStudio.Core/Services/CacheService.cs
@@ -18,7 +18,8 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
                 return true;
 
             return await this.TryPutAsync(fullPath,
-                new BundleCache(File.OpenRead(fullPath).Length, await this.GetFileMD5Async(fullPath), serialized));
+                new BundleCache(new FileInfo(fullPath).Length, await this.GetFileMD5Async(fullPath), serialized),
+                false);
         }
 
         public async Task<(bool, SerializedFile)> TryGetCachedFileAsync(string filePath) {
@@ -30,18 +31,23 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
         }
 
         private async Task<(bool, SerializedFile)> IsFileCached(string filePath) {
-            var (res, cache) = await TryGetValue<BundleCache>(filePath);
+            var (res, bundle) = await TryGetValue<BundleCache>(filePath);
             if (res) {
-                if (cache.Size == File.OpenRead(filePath).Length) {
-                    if (cache.MD5 == await GetFileMD5Async(filePath))
-                        return (true, cache.Serialized);
+                if (bundle.Size == new FileInfo(filePath).Length) {
+                    if (bundle.MD5 == await GetFileMD5Async(filePath))
+                        return (true, bundle.Serialized);
                 }
             }
 
             return (false, null);
         }
 
-        private async Task<string> GetFileMD5Async(string filePath) {
+#if DEBUG
+        public
+#else
+        private
+#endif
+            async Task<string> GetFileMD5Async(string filePath) {
             Stream stream = File.OpenRead(filePath);
             if (stream.Length > 1000000) {
                 var tmp = new byte[1000000];
@@ -52,29 +58,32 @@ namespace SoarCraft.QYun.UnityABStudio.Core.Services {
             return BitConverter.ToString(await MD5.Create().ComputeHashAsync(stream)).Replace("-", "");
         }
 
-        public Task<bool> TryPutAsync(object key, object value, int seconds = byte.MaxValue) => Task.Run(() => {
-            if (this.cache.TryGetValue(key, out _))
-                return false;
+        public Task<bool> TryPutAsync(object key, object value, bool checkExist = true, int seconds = byte.MaxValue) =>
+            Task.Run(() => {
+                if (checkExist) {
+                    if (this.cache.TryGetValue(key, out _))
+                        return false;
+                }
 
-            _ = this.cache.Set(key, value, TimeSpan.FromSeconds(seconds));
-            return true;
-        });
+                _ = this.cache.Set(key, value, TimeSpan.FromSeconds(seconds));
+                return true;
+            });
 
         public Task<(bool, T)> TryGetValue<T>(object key) where T : class => Task.Run(() =>
             this.cache.TryGetValue(key, out var res) ? (true, res as T) : (false, null));
 
         public void Remove(object key) => this.cache.Remove(key);
-    }
 
-    internal class BundleCache {
-        public long Size;
-        public string MD5;
-        public SerializedFile Serialized;
+        private class BundleCache {
+            public long Size;
+            public string MD5;
+            public SerializedFile Serialized;
 
-        public BundleCache(long size, string md5, SerializedFile serialized) {
-            this.Size = size;
-            this.MD5 = md5;
-            this.Serialized = serialized;
+            public BundleCache(long size, string md5, SerializedFile serialized) {
+                this.Size = size;
+                this.MD5 = md5;
+                this.Serialized = serialized;
+            }
         }
     }
 }
